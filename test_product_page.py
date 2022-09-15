7c173873c3fad26605b3ab7f9655e3338294b4c5
vdesyatke <72273263+vdesyatke@users.noreply.github.com>
2022-09-15 22:17:43 +0400

    marked two intentionally failing tests as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -34,7 +34,7 @@ def test_guest_can_add_product_to_basket(browser, link_nr):
 # Открываем страницу товара
 # Добавляем товар в корзину
 # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
-# @pytest.mark.xfail
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -58,7 +58,7 @@ def test_guest_cant_see_success_message(browser):
 # Открываем страницу товара
 # Добавляем товар в корзину
 # Проверяем, что нет сообщения об успехе с помощью is_disappeared
-# @pytest.mark.xfail
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
