01e5787644425d32b95d3b71c656131c2c8ee9e9
vdesyatke <72273263+vdesyatke@users.noreply.github.com>
2022-09-26 12:24:49 +0400

    deleted all commented code blocks

--- test_product_page.py
+++ test_product_page.py
@@ -9,8 +9,6 @@ import pytest
 import time
 
 
-# @pytest.mark.parametrize('link_nr', list(range(0,10)))
-# @pytest.mark.skip(reason="Too long to wait for 10 pages open, pass quiz and add item to basket")
 @pytest.mark.parametrize('link_nr', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 def test_guest_can_add_product_to_basket(browser, link_nr):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -91,7 +89,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
                                                       :18] == 'Ваша корзина пуста', 'Basket should be empty'
 
 
-@pytest.mark.second_stage
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, browser):
@@ -109,7 +106,6 @@ class TestUserAddToBasketFromProductPage:
         login_page.register_new_user(email=email, password=password)
         assert login_page.is_element_present(*BasePageLocators.USER_ICON), 'No user icon found, seems like no user logged in'
 
-
     def test_user_cant_see_success_message(self, browser):
         link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
@@ -123,48 +119,3 @@ class TestUserAddToBasketFromProductPage:
         page.add_to_basket(quiz_solve=False)
         assert page.addable_item_name == page.item_name_in_alert_message
         assert page.addable_item_price == page.amount_of_basket_in_alert_message
-
-
-# @pytest.mark.second_stage
-# def test_user_cant_see_success_message(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     login_link = page.browser.find_element(*BasePageLocators.LOGIN_LINK)
-#     login_link.click()
-#     login_page = LoginPage(browser, browser.current_url)
-#     assert login_page.should_be_login_page(), 'Seems like this is not login page'
-#     email = str(time.time()) + '@fakemail.org'
-#     password = '9023719528'
-#     login_page.register_new_user(email=email, password=password)
-#     assert login_page.is_element_present(
-#         *BasePageLocators.USER_ICON), 'No user icon found, seems like no user logged in'
-#
-#     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser, link)
-#     page.open()
-#     assert page.is_not_element_present(*ProductPageLocators.INNER_ALERT_MESSAGE_ITEM_ADDED_SUCCESS)
-#
-# @pytest.mark.second_stage
-# def test_user_can_add_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     login_link = page.browser.find_element(*BasePageLocators.LOGIN_LINK)
-#     login_link.click()
-#     time.sleep(10)
-#     login_page = LoginPage(browser, browser.current_url)
-#     assert login_page.should_be_login_page(), 'Seems like this is not login page'
-#     email = str(time.time()) + '@fakemail.org'
-#     password = '9023719528'
-#     login_page.register_new_user(email=email, password=password)
-#     assert login_page.is_element_present(
-#         *BasePageLocators.USER_ICON), 'No user icon found, seems like no user logged in'
-#
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_basket(quiz_solve=False)
-#     assert page.addable_item_name == page.item_name_in_alert_message
-#     assert page.addable_item_price == page.amount_of_basket_in_alert_message
-#
